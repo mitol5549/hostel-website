83d2fae4dbf7d6ac9e2d4f3ae48b4a725cc944b7
Andrey Osipov <mitol5549@gmail.com>
2025-01-22 15:11:25 +0100

    footer changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import { FOOTER_CONTACT_INFO, SOCIALS } from '@/Constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import YandexMap from '@/components/YandexMap';
+import { Divider } from '@heroui/react';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -49,10 +50,12 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="border bg-gray-20" />
-        <p className="regular-14 w-full text-center text-gray-30">
-          {currentYear} "Тихий" хостел | Powered by NextJS 14
-        </p>
+        <Divider className="my-4" />
+        <div className="w-full justify-center text-gray-30 flex h-5 items-center space-x-4 ">
+          <p className="regular-14">{currentYear} "Тихий" хостел</p>
+          <Divider orientation="vertical" />
+          <p className="regular-14">Powered by NextJS 14</p>
+        </div>
       </div>
     </footer>
   );
