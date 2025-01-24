8d4e23482e7beba42a6eda313b947b60f186061e
Andrey Osipov <mitol5549@gmail.com>
2025-01-24 18:45:19 +0100

    room card fixed, images fixed

--- components/RoomCard.tsx
+++ components/RoomCard.tsx
@@ -1,6 +1,5 @@
 import { Card, CardFooter, CardHeader, Image } from '@heroui/react';
 import Button from './Button';
-import RoomGallery from './RoomGallery';
 
 interface RoomCardProps {
   images: string[];
@@ -12,19 +11,27 @@ interface RoomCardProps {
 
 const RoomCard: React.FC<RoomCardProps> = ({ images, title, description, price, capacity }) => {
   return (
-    <Card className="w-full h-[300px] col-span-12 sm:col-span-7">
-      <CardHeader className="absolute z-10 top-1 flex-col items-start">
-        <h3 className="font-bold text-xl mb-2">{title}</h3>
-        <p className="text-gray-600 text-sm mb-4">{description}</p>
-      </CardHeader>
-      <Image removeWrapper alt="room" className="z-0 w-full h-full object-cover" src={images[0]} />
-      <CardFooter>
-        <div className="flex justify-between items-center mb-4">
-          <span className="text-green-600 font-bold">{price} ₽ / ночь</span>
-          <span className="text-gray-500">Вместимость: {capacity} чел.</span>
-        </div>
-        <Button type="button" title="Забронировать" variant="btn_dark_green" />
-      </CardFooter>
+    <Card className="w-full h-[300px] col-span-12 sm:col-span-7 flex flex-row">
+      {/* Фото слева */}
+      <div className="w-1/2 h-full">
+        <Image removeWrapper alt="room" className="w-full h-full object-cover" src={images[0]} />
+      </div>
+
+      {/* Информация справа */}
+      <div className="w-1/2 p-6 flex flex-col justify-between">
+        <CardHeader className="mb-4 flex flex-col">
+          <h3 className="font-bold text-xl mb-2">{title}</h3>
+          <p className="text-gray-600 text-sm">{description}</p>
+        </CardHeader>
+
+        <CardFooter className="flex flex-col">
+          <div className="flex flex-col justify-between items-center mb-4">
+            <span className="text-green-600 font-bold">{price} ₽ / ночь</span>
+            <span className="text-gray-500">Вместимость: {capacity} чел.</span>
+          </div>
+          <Button type="button" title="Забронировать" variant="btn_dark_green" />
+        </CardFooter>
+      </div>
     </Card>
   );
 };
