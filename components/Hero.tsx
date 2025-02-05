94ab4c811dd0c114027159240ec819e6207b4389
Andrey Osipov <mitol5549@gmail.com>
2025-02-05 13:59:25 +0100

    widget test

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,32 @@
 'use client';
 
 import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-// import Button from './Button'
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
+
+const HotelBookingWidget = dynamic(() => import('@/components/HotelWidget'), { ssr: false });
 
 const Hero = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && (window as any).HotelWidget) {
+      (window as any).HotelWidget.add({
+        type: 'bookingForm',
+        appearance: { container: 'WidgetHorizontalBlockId' },
+      });
+    }
+  }, []);
+
   return (
     <section
-      id="#home"
+      id="home"
       className="relative h-screen max-container padding-container flex flex-col
     gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row justify-center items-center"
     >
       <div className="relative mt-48 z-20 flex flex-1 flex-col xl:w-1/2">
         <h1 className="bold-52 lg:bold-88">Хостел "Тихий"</h1>
-
         <div className="my-11 flex flex-wrap gap-5">
           <p className="regular-16 mt-6 bg-white/80 text-gray-30 xl:max-w-[520px]">
-            Хостел “Тихий” — новый уютный хостел, расположенный по адресу переулок Тихий, 2, в тихом и удобном районе
+            Хостел "Тихий" — новый уютный хостел, расположенный по адресу переулок Тихий, 2, в тихом и удобном районе
             Красноярска. Близость к ключевым транспортным узлам и городской инфраструктуре делает его отличным выбором
             для путешественников и гостей города. Интерьер хостела выполнен в современном дизайнерском стиле с
             элементами, отражающими природу и атмосферу уюта. Мы позаботились о том, чтобы каждый гость чувствовал себя
@@ -28,7 +38,7 @@ const Hero = () => {
       <div className="relative flex-1 items-start hidden xl:flex">
         <div
           className="relative z-20 flex w-[260px] flex-col gap-8
-        rounded-3xl bg-green-700 px-7 py-8"
+          rounded-3xl bg-green-700 px-7 py-8"
         >
           <div className="flex flex-col">
             <div className="flexBetween">
@@ -50,6 +60,9 @@ const Hero = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="w-full flex justify-center">
+        <HotelBookingWidget />
       </div>
     </section>
   );
