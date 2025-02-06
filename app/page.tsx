688b090b7ab09af7dd41df548c2d41344b288ce2
Andrey Osipov <mitol5549@gmail.com>
2025-02-06 14:19:09 +0100

    deployment test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import Camp from '@/components/Camp';
 import Features from '@/components/Features';
 import Hero from '@/components/Hero';
 import RoomList from '@/components/RoomList';
 import Image from 'next/image';
+import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -17,7 +20,6 @@ export default function Home() {
           priority
           className="z-0"
         />
-        {/* <div className="absolute inset-0 bg-black bg-opacity-50 z-10" /> */}
         <div className="relative z-20">
           <Hero />
         </div>
@@ -25,6 +27,31 @@ export default function Home() {
       <RoomList />
       <Features />
       <Camp />
+      <div id="showCheckAvailabilityButtonForMobileDevices" />
+
+      <Script
+        src="https://bookonline24.ru/widget.js"
+        onReady={() => {
+          window.HotelWidget?.init({
+            hotelId: 'e2877d32-3e03-4011-926a-79404e1b490a',
+            version: '2',
+            baseUrl: 'https://bookonline24.ru/',
+            hooks: {
+              onError: (e: any) => console.error('onError', e),
+              onInit: () => console.log('onInit'),
+              onBooking: (v: any) => console.log('onBooking', v),
+            },
+          });
+
+          window.HotelWidget?.add({
+            type: 'bookingForm',
+            inline: true,
+            appearance: {
+              container: 'WidgetHorizontalBlockId',
+            },
+          });
+        }}
+      />
     </>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,44 +1,28 @@
-'use client';
-
 import Image from 'next/image';
-import { useEffect } from 'react';
-import dynamic from 'next/dynamic';
-
-const HotelBookingWidget = dynamic(() => import('@/components/HotelWidget'), { ssr: false });
 
 const Hero = () => {
-  useEffect(() => {
-    if (typeof window !== 'undefined' && (window as any).HotelWidget) {
-      (window as any).HotelWidget.add({
-        type: 'bookingForm',
-        appearance: { container: 'WidgetHorizontalBlockId' },
-      });
-    }
-  }, []);
-
   return (
     <section
       id="home"
       className="relative h-screen max-container padding-container flex flex-col
-    gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row justify-center items-center"
+      gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row justify-center items-center"
     >
+      {/* Основной контент */}
       <div className="relative mt-48 z-20 flex flex-1 flex-col xl:w-1/2">
-        <h1 className="bold-52 lg:bold-88">Хостел "Тихий"</h1>
+        <h1 className="font-sofia font-extrabold text-4xl">Хостел "Тихий"</h1>
         <div className="my-11 flex flex-wrap gap-5">
           <p className="regular-16 mt-6 bg-white/80 text-gray-30 xl:max-w-[520px]">
-            Хостел "Тихий" — новый уютный хостел, расположенный по адресу переулок Тихий, 2, в тихом и удобном районе
-            Красноярска. Близость к ключевым транспортным узлам и городской инфраструктуре делает его отличным выбором
-            для путешественников и гостей города. Интерьер хостела выполнен в современном дизайнерском стиле с
-            элементами, отражающими природу и атмосферу уюта. Мы позаботились о том, чтобы каждый гость чувствовал себя
-            как дома.
+            Хостел «Тихий» предлагает комфортное размещение для путешественников и деловых людей. Мы находимся в центре
+            города, недалеко от основных достопримечательностей и транспортных узлов.
           </p>
         </div>
       </div>
 
+      {/* Дополнительный блок */}
       <div className="relative flex-1 items-start hidden xl:flex">
         <div
           className="relative z-20 flex w-[260px] flex-col gap-8
-          rounded-3xl bg-green-700 px-7 py-8"
+        rounded-3xl bg-green-700 px-7 py-8"
         >
           <div className="flex flex-col">
             <div className="flexBetween">
@@ -61,8 +45,10 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="w-full flex justify-center">
-        <HotelBookingWidget />
+
+      {/* Делаем блок всегда снизу */}
+      <div className="w-full flex justify-center mt-auto">
+        <div id="WidgetHorizontalBlockId" />
       </div>
     </section>
   );
