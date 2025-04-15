be21dba305462f7f1be8d143e8e6c0f249e7fcee
Andrey Osipov <mitol5549@gmail.com>
2025-04-15 10:49:00 +0200

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 
 import MainHeader from '@/components/MainHeader';
-import Footer from '@/components/Footer';
 import { Providers } from './providers';
 
 import './globals.css';
+import Footer from '@/components/Footer';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -14,10 +14,38 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: 'Хостел Тихий',
-  description: 'Хостел "Тихий" в Красноярске',
+  metadataBase: new URL('https://xn--e1agaehq2ac2a1e.xn--p1ai'),
+  title: 'Хостел "Тихий" в Красноярске',
+  description:
+    'Комфортное размещение в центре Красноярска с бесплатным Wi-Fi, круглосуточной регистрацией и другими удобствами.',
   icons: {
     icon: '/x-logo.svg',
+  },
+  keywords: [
+    'хостел',
+    'Красноярск',
+    'комфортное проживание',
+    'бесплатный Wi-Fi',
+    'круглосуточная регистрация',
+  ],
+  authors: [{ name: 'Хостел "Тихий"' }],
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Хостел "Тихий" в Красноярске',
+    description:
+      'Ищете комфортное и доступное проживание в центре Красноярска? Хостел "Тихий" предлагает уютные номера с бесплатным Wi-Fi и круглосуточной регистрацией.',
+    url: 'https://xn--e1agaehq2ac2a1e.xn--p1ai/',
+    siteName: 'Хостел "Тихий"',
+    images: [
+      {
+        url: 'https://xn--e1agaehq2ac2a1e.xn--p1ai/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Фасад хостела "Тихий"',
+      },
+    ],
+    locale: 'ru_RU',
+    type: 'website',
   },
 };
 
---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://xn--e1agaehq2ac2a1e.xn--p1ai',
+  generateRobotsTxt: true,
+};
